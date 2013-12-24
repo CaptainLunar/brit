932c74ddedd9421c7e6f86232493b52629fe0b34
CaptainLunar <jeanmalmonte@yahoo.com>
2013-12-24 08:32:59 -0600

    Update brit.py

--- brit.py
+++ brit.py
@@ -2,16 +2,20 @@
 brit.py - Phenny Rude Module
 Copyright 2012, Captain Lunar 
 Licensed under the Eiffel Forum License 2.
-http://sites.google.com/site/captainlunarlabs/
-
 http://inamidst.com/phenny/
+This is a nano function to call random British swears into an IRC channel phenny is located on command. 
 """
-#This is a nano function to call random British swears into the IRC channel on command. 
 
 import random
 
-swears = ["Your mom's a slag faggot!", "Wank my ass in your face cunt!", "Rude!", "You binbongy cunt, go drink your mom's tats you fag!", "You're such a uphill gardener you queer!",
-         "Fuck your shit, Amerifat.", "I'm gonna go drink tea with Dark, because we're British #swagger."]
+swears = ["Your mom's a slag faggot!", 
+	  "Wank my ass in your face cunt!", 
+	  "Rude!", 
+	  "You binbongy cunt, go drink your mom's tats you fag!", 
+	  "You're such a uphill gardener you queer!",
+          "Fuck your shit, Amerifat.", 
+          "I'm gonna go drink tea with Dark, because we're British #swagger."]
+          #You can also define your own rude swears here!
 
 def brit(phenny, input):
 	rude = "%s" % str(random.choice(swears))
